4ae2cb4cf3c4f8490408e4e5f43448c16f50eeb6
sugan0tech <sugankpms@gmail.com>
2022-09-19 03:53:29 +0530

    Primary Beans

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,11 +1,21 @@
 package guru.springframework.sfgdi.controllers;
 
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
-    public String sayHello(){
-        System.out.println("Hello World");
-        return "Hi Folks!";
+    private  final GreetingService greetingService;
+
+    @Autowired
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
     }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+
+
 }
